dc14582511b1bac519ec17123355067106d6f089
diazzzu <diazzz.usenov@gmail.com>
2022-01-26 19:42:03 +0300

    Initial Commit

---
+++ Apple pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple pie
+//
+//  Created by Dias Ussenov on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
